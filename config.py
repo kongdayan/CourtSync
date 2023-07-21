6f38516192b09c12a0d6a591c7fa32f50dfc1829
kongwenyan <kongwenyancn@gmail.com>
2023-07-21 16:10:47 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,25 @@
+user_header = {
+    "Content-Length":"",
+    "Content-Type":" application/x-www-form-urlencoded",
+    "Cookie":"JSESSIONID=6994D9213EA26BCF37F91C6BA53C7BE3; _ga_3E3P4SB1VP=GS1.1.1688612949.2.1.1688614193.0.0.0; _ga_9CD5KG37Y0=GS1.2.1689233968.4.0.1689233968.0.0.0; _ga=GA1.1.1529978837.1685687810; _ga_MB3NM58GP2=GS1.1.1689759423.3.0.1689759423.0.0.0",
+    "Host":" w6.ab.ust.hk",
+}
+
+location_dict = {
+    "100": "SF-C1",
+    "101": "SF-C2",
+    "2": "LG1-C1",
+    "3": "LG1-C2",
+    "4": "LG1-C3",
+    "5": "LG1-C4",
+    "79": "LG1-C5",
+    "80": "LG1-C6",
+}
+
+push_config = {
+    "server":"https://api2.pushdeer.com/message/push?pushkey={key}&text="
+}
+
+server_config = {
+    "SCAN_URL":"https://w6.ab.ust.hk/fbs_user/bin2/Timetable"
+}
